895826a2b27896e53e3b07df4c8c06e66268d195
Tim Upthegrove <tupty@bbn.com>
2013-11-02 13:52:24 -0400

    Remove old source files

--- src/tuptyslicer/FlowmodVirtualizable.java
+++
@@ -1,8 +0,0 @@
-package tuptyslicer;
-
-import org.openflow.protocol.OFFlowMod;
-
-public interface FlowmodVirtualizable {
-
-	public OFFlowMod virtualizeFlowmod(OFFlowMod flowmod, Slicelet slicelet);
-}
